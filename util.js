6119af07342932fc79ac0460ca189398b14877aa
gtdalp <295194117@qq.com>
2016-10-20 22:59:21 +0800

    add inputChange.html demo and  update inputChange

--- util.js
+++ util.js
@@ -55,12 +55,13 @@ var XSUtil = {
     *
     *************************/
     inputChange : function( ele, cb ) {
-        var dom = $("#"+ele)[0];
-        if(dom == undefined) return;
-        if( "\v" == "v" ) {
-            dom.onpropertychange = cb;
-        }else{
-            dom.addEventListener("input", cb, false);
+        var dom = document.querySelector(ele);
+        if (dom) {
+            if( "\v" == "v" ) {
+                dom.onpropertychange = cb;
+            }else{
+                dom.addEventListener("input", cb, false);
+            }
         }
         return cb;
     },
