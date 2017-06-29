63944f4d0e429c9475b9117b647ddab96e64494e
xisa <295194117@qq.com>
2017-06-29 15:29:07 +0800

    Update util.js

--- util.js
+++ util.js
@@ -666,6 +666,99 @@ let XSUtil = {
         }
         fn();
     }
-    
+    extend: function() {
+      let isWindow = function( obj ) {
+        return obj != null && obj == obj.window;
+      }
+      let isPlainObject = function( obj ) {
+        var key;
+        let core_hasOwn = Object.hasOwnProperty;
+        // Must be an Object.
+        // Because of IE, we also have to check the presence of the constructor property.
+        // Make sure that DOM nodes and window objects don't pass through, as well
+        if ( !obj || typeof obj !== "object" || obj.nodeType || isWindow( obj ) ) {
+          return false;
+        }
+
+        try {
+          // Not own constructor property must be Object
+          if ( obj.constructor &&
+            !core_hasOwn.call(obj, "constructor") &&
+            !core_hasOwn.call(obj.constructor.prototype, "isPrototypeOf") ) {
+            return false;
+          }
+        } catch ( e ) {
+          // IE8,9 Will throw exceptions on certain host objects #9897
+          return false;
+        }
+
+        // Own properties are enumerated firstly, so to speed up,
+        // if last one is own, then all properties are own.
+        for ( key in obj ) {}
+
+        return key === undefined || core_hasOwn.call( obj, key );
+      };
+
+      var src, copyIsArray, copy, name, options, clone,
+        target = arguments[0] || {},
+        i = 1,
+        length = arguments.length,
+        deep = false;
+
+      // Handle a deep copy situation
+      if ( typeof target === "boolean" ) {
+        deep = target;
+        target = arguments[1] || {};
+        // skip the boolean and the target
+        i = 2;
+      }
+
+      // Handle case when target is a string or something (possible in deep copy)
+      if ( typeof target !== "object" && !(typeof target == 'function') ) {
+        target = {};
+      }
+
+      if ( length === i ) {
+        target = this;
+        --i;
+      }
+
+      for ( ; i < length; i++ ) {
+        // Only deal with non-null/undefined values
+        if ( (options = arguments[ i ]) != null ) {
+          // Extend the base object
+          for ( name in options ) {
+            src = target[ name ];
+            copy = options[ name ];
+
+            // Prevent never-ending loop
+            if ( target === copy ) {
+              continue;
+            }
+
+            // Recurse if we're merging plain objects or arrays
+            if ( deep && copy && ( isPlainObject(copy) || (copyIsArray = Array.isArray(copy)) ) ) {
+              if ( copyIsArray ) {
+                copyIsArray = false;
+                clone = src && Array.isArray(src) ? src : [];
+
+              } else {
+                clone = src && isPlainObject(src) ? src : {};
+              }
+
+              // Never move original objects, clone them
+              target[ name ] = XSUtil.extend( deep, clone, copy );
+
+            // Don't bring in undefined values
+            } else if ( copy !== undefined ) {
+              target[ name ] = copy;
+            }
+          }
+        }
+      }
+
+      // Return the modified object
+      return target;
+    }
 };
 window.XSUtil = XSUtil;
